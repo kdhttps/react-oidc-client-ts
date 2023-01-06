4d0a34686bc48ec27ed3a7086da38d5a230a2142
kdhttps <kdhttps@gmail.com>
2023-01-06 14:38:02 +0530

    feat: add token introspection

--- src/routes/dashboard.jsx
+++ src/routes/dashboard.jsx
@@ -1,14 +1,29 @@
+import { useEffect } from "react";
 import { makeUserAuthentication } from "../makeOIDCUserManager";
 
 export default function Dashboard() {
   const auth = makeUserAuthentication()
 
-  async function getToken() {
-    let accessToken = await auth.getAccessToken()
-    console.log("Dashboard accessToken: ", accessToken)
-  }
+  useEffect(() => {
+    introspectToken()
+  })
 
-  getToken()
+  async function introspectToken() {
+    const accessToken = await auth.getAccessToken()
+    console.log("Dashboard accessToken: ", accessToken)
+
+    const formBody = `${encodeURIComponent("token")}=${encodeURIComponent(accessToken)}`
+    const response = await fetch(`${process.env.REACT_APP_OP_SERVER}/jans-auth/restv1/introspection`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': `Bearer ${accessToken}`
+      },
+      body: formBody
+    });
+
+    console.log(await response.json())
+  }
 
   return (
     <div id="dashboard-page">
